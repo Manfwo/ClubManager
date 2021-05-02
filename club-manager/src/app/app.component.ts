b53168b1ddf6938fb798f66d0909b9f091e1e8e0
MW <wolffm@gmx.net>
2021-05-02 17:20:01 +0200

    implementierung Local Stoarge

--- club-manager/src/app/app.component.ts
+++ club-manager/src/app/app.component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Inject} from '@angular/core';
+import { DOCUMENT} from '@angular/common';
+import { ThemeSwitchComponent } from './general/theme-switch/theme-switch.component';
+import { LocalStorageService } from './_shared/local-storage.service';
+
 @Component({
   selector: 'cl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) private document: Document, private localStorageService: LocalStorageService) {
+  }
   public title = 'Club-Manager';
   public isAuthenticated = true;
+  public isShowing = true;
   public isExpanded = false;
-  public menuSize = 'minMenu';
   public menuButton = 'minMenuButton';
+  public menuSize = 'minMenu';
 
-  // Text oder Menü
+  ngOnInit(): void {
+    // Theme setzen
+    const switchTheme = new ThemeSwitchComponent(this.document, this.localStorageService);
+    console.log('setting', this.localStorageService.get('theme'));
+    if (this.localStorageService.get('theme') === 'light') {
+      switchTheme.selectLightTheme();
+    }
+    else {
+      switchTheme.selectDarkTheme();
+    }
+    // Icon oder Icon-Text Menü
+    this.setMenu(this.localStorageService.get('expandMenu'));
+    // Open/Close Menü
+    this.isShowing = this.localStorageService.get('showMenu');
+  }
+
+  // Menü anzeigen
+  public toggleSidenav(): void  {
+    this.isShowing = !this.isShowing;
+    this.localStorageService.set('showMenu', this.isShowing);
+  }
+
+  // Icon oder Icon Text Menü
   public toggleMenu(): void {
+    this.isShowing = false;
     this.isExpanded = !this.isExpanded;
-    if (this.isExpanded) {
+    this.localStorageService.set('expandMenu', this.isExpanded);
+    this.setMenu(this.isExpanded);
+    this.isShowing = true;
+  }
+
+  setMenu( expand: boolean): void {
+    this.isExpanded = expand;
+    if (expand) {
       this.menuSize = 'maxMenu';
       this.menuButton = 'maxMenuButton';
     }
--- club-manager/src/app/general/theme-switch/theme-switch.component.ts
+++ club-manager/src/app/general/theme-switch/theme-switch.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
+import { LocalStorageService } from '../../_shared/local-storage.service';
 
 @Component({
   selector: 'cl-theme-switch',
@@ -10,23 +11,25 @@ export class ThemeSwitchComponent {
 
     private static readonly DARK_THEME_CLASS = 'dark-theme';
     private static readonly LIGHT_THEME_CLASS = 'light-theme';
-    private static readonly DARK_THEME_LIGHT = 'light';
-    private static readonly DARK_THEME_DARK = 'dark';
+    private static readonly THEME_LIGHT = 'light';
+    private static readonly THEME_DARK = 'dark';
 
     public theme: string;
 
-    constructor(@Inject(DOCUMENT) private document: Document) {
+    constructor(@Inject(DOCUMENT) private document: Document, private localStorageService: LocalStorageService) {
       this.theme = this.document.documentElement.classList.contains(ThemeSwitchComponent.DARK_THEME_CLASS) ?
-                   ThemeSwitchComponent.DARK_THEME_DARK : ThemeSwitchComponent.DARK_THEME_LIGHT;
+                   ThemeSwitchComponent.THEME_DARK : ThemeSwitchComponent.THEME_LIGHT;
     }
 
     public selectDarkTheme(): void {
         this.document.documentElement.classList.replace(ThemeSwitchComponent.LIGHT_THEME_CLASS, ThemeSwitchComponent.DARK_THEME_CLASS);
-        this.theme = ThemeSwitchComponent.DARK_THEME_DARK;
+        this.theme = ThemeSwitchComponent.THEME_DARK;
+        this.localStorageService.set('theme', ThemeSwitchComponent.THEME_DARK);
     }
 
     public selectLightTheme(): void {
         this.document.documentElement.classList.replace(ThemeSwitchComponent.DARK_THEME_CLASS, ThemeSwitchComponent.LIGHT_THEME_CLASS);
-        this.theme = ThemeSwitchComponent.DARK_THEME_LIGHT;
+        this.theme = ThemeSwitchComponent.THEME_LIGHT;
+        this.localStorageService.set('theme', ThemeSwitchComponent.THEME_LIGHT);
     }
 }
