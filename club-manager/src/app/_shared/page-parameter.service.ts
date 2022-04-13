ba9c7a5f993e26e24566c9be963daae62b7836c6
MW <wolffm@gmx.net>
2022-04-13 20:33:49 +0200

    Externe ID + Implemtierung Paginator in Fußzeile

---
+++ club-manager/src/app/_shared/page-parameter.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { PageParameter } from './page-parameter';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PageParameterService {
+
+  private param = new BehaviorSubject<PageParameter>(new PageParameter());
+
+  public sharedPageParameter = this.param.asObservable();
+
+  constructor() { }
+
+  public nextMessage( p: PageParameter) {
+    console.log("PAGE-SERVICE",p);
+    this.param.next(p)
+  };
+}
